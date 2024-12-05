17f1c9323acdcde90ad78f298ee03a33fc49b04c
fredstech-ik <fredtech650@gmail.com>
2024-12-05 13:58:53 +0100

    added a coment to car.js

--- car.js
+++ car.js
@@ -1,3 +1,4 @@
+/* all javascript code */
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
